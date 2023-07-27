c42890d782f094d571ec35e83d37056672f2f661
hjung43 <140682638+hjung43@users.noreply.github.com>
2023-07-27 14:06:20 +0900

    [Bronze V] Title: 꼬마 정민, Time: 112 ms, Memory: 12884 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        Long A = sc.nextLong();
+        Long B = sc.nextLong();
+        Long C = sc.nextLong();
+        Long D = A+B+C;
+        
+        System.out.println(D);
+        
+
+	}
+
+}
