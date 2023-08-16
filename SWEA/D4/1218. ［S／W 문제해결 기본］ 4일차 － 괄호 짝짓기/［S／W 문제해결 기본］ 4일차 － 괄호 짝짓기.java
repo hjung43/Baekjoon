3ac898289f70595280cbcefe3385a5975d22a56d
hjung43 <140682638+hjung43@users.noreply.github.com>
2023-08-16 14:12:54 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 144 ms, Memory: 20,976 KB -BaekjoonHub

--- SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
@@ -5,6 +5,7 @@ public class Solution {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+//		int T = sc.nextInt();
 		//테스트 케이스 10번 반복하기
 		for (int t = 1; t <= 10; t++) {
 			Stack<Character> stack = new Stack<>(); //숫자를 담을 스택 생성
@@ -14,27 +15,19 @@ public class Solution {
 			for(int i=0; i<k; i++) {
 				char c = str.charAt(i); //괄호 문자로 나누기
 				if(c==')') { //닫는 소괄호 일때 일때
-					if('('!=stack.peek()){ //stack의 top이 (가 아니면
-						break;  //멈추기
-					}else  //맞으면
-						stack.pop(); //(지우기
+					if('('!=stack.pop()) //stack의 top이 (가 아닌지 비교 & top삭제
+						break;  //아니면 멈추기 (맞으면 top만 삭제)
 				}else if(c=='}') { //닫는 중괄호 일때
-					if('{'!=stack.peek()) { //stack의 top이 {가 아니면
-						break;  //멈추기
-					}else  //맞으면
-						stack.pop(); //{지우기
+					if('{'!=stack.pop()) //stack의 top이 {가 아 아닌지 비교 & top삭제
+						break;  //아니면 멈추기 (맞으면 top만 삭제)
 				}else if(c==']') { //닫는 대괄호 일때
-					if('['!=stack.peek()){ //stack의 top이 [가 아니면
-						break;  //멈추기
-					}else  //맞으면
-						stack.pop(); //[지우기
+					if('['!=stack.pop()) //stack의 top이 [가 아 아닌지 비교 & top삭제
+						break;  //아니면 멈추기 (맞으면 top만 삭제)
 				}else if(c=='>') { //닫는 화살괄호 일때
-					if('<'!=stack.peek()){ //stack의 top이 <가 아니면
-						break;   //멈추기
-					}else  //맞으면
-						stack.pop(); //<지우기
+					if('<'!=stack.pop()) //stack의 top이 <가 아 아닌지 비교 & top삭제
+						break;   //아니면 멈추기  (맞으면 top만 삭제)
 				}else { //닫는괄호가 아닐때
-					stack.push(c); //스택에 넣기
+					stack.push(c); //여는괄호 스택에 넣기
 				}
 			}
 			if(stack.isEmpty())  //스택이 비어있으면
