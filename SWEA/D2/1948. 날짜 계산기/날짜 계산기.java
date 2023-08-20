7ee4d1a24a7196b51e976a78e27f707b0470a8d9
hjung43 <140682638+hjung43@users.noreply.github.com>
2023-08-20 20:28:53 +0900

    [D2] Title: 날짜 계산기, Time: 128 ms, Memory: 20,464 KB -BaekjoonHub

--- SWEA/D2/1948. 날짜 계산기/날짜 계산기.java
+++ SWEA/D2/1948. 날짜 계산기/날짜 계산기.java
@@ -1,18 +1,29 @@
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] month = {0,31,59,90,120,151,181,212,243,273,304,334,365};
+		int[] month = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 		int T = sc.nextInt();
 		for(int t=1; t<=T; t++) {
-			int m1 = sc.nextInt();
-			int d1 = sc.nextInt();
-			int m2 = sc.nextInt();
-			int d2 = sc.nextInt();
-			int result = month[m2-1] - month[m1-1] + d2 - d1 +1;
-			System.out.printf("#%d %d\n",t, result);
+			int m1 = sc.nextInt();  //첫번째 날짜 월
+			int d1 = sc.nextInt();  //첫번째 날짜 일
+			int m2 = sc.nextInt();  //두번째 날짜 월
+			int d2 = sc.nextInt();  //두번째 날짜 일
+			int day1 = 0;   //첫번째 날짜 까지 모든 일수 더한 값 저장할 변수
+            //1월1일 부터 첫번째 날짜까지의 모든 일 수
+			for(int i=0; i<m1-1; i++) {
+				day1 += month[i];
+			}
+			day1 += d1;
+			int day2 = 0;  //두번째 날짜 까지 모든 일 수 더한 값 저장할 변수
+             //1월1일 부터 두번째 날짜 까지의 모든 일 수
+			for(int i=0; i<m2-1; i++) {
+				day2 += month[i];
+			}
+			day2 += d2;
+			
+			System.out.printf("#%d %d\n",t,day2-day1+1);
 		}
 	}
 }
