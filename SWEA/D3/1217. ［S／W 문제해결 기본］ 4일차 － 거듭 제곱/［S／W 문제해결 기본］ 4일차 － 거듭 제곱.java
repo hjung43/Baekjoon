764eb3aa75840a4b5d477fb2d8076378ceefed47
hjung43 <140682638+hjung43@users.noreply.github.com>
2023-08-31 15:57:43 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 131 ms, Memory: 21,064 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -1,21 +1,26 @@
 import java.util.Scanner;
 
 public class Solution {
-
+	public static int[]arr = new int[1000000];
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		//테스트 케이스 10번 반복
-		for(int t=0; t<10; t++) {
-			int T = sc.nextInt(); //테스트 케이스 번호 받기
-			int n = sc.nextInt(); //제곱할 밑수 입력
-			int m = sc.nextInt(); //지수 입력
-			System.out.printf("#%d %d\n",T,power(n,m)); //거듭제곱 함수 호출 후 출력
+		for(int i=0; i<10; i++) {
+			int T = sc.nextInt();
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			
+			System.out.println("#"+T+" "+power(n,m));
 		}
 	}
-	//거듭제곱 함수
-	public static int power (int n, int m) { //n=밑수 , m=지수
-		if(m==1) return n;      //지수가 1이되면 밑수 리턴
-		return n*power(n,m-1);  //재귀함수 사용
-								//2의5승 = 2*2의4승 -> 2의4승 = 2*2의3승 ... 
+	public static int power (int x,int n) {
+		if(n==1) return x;
+		if(n%2==0) {
+			int r = power(x,n/2);
+			return r*r;
+		}else {
+			int r = power(x,(n-1)/2);
+			return r*r*x;
+		}
 	}
+
 }
