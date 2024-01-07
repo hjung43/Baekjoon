91d659f87fb7c3baf9d21957e16ddaf68026d35b
hjung43 <140682638+hjung43@users.noreply.github.com>
2024-01-08 02:25:06 +0900

    [Silver I] Title: 그림, Time: 368 ms, Memory: 51780 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,10 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -40,16 +36,17 @@ public class Main {
 		//그림 찾기
 		for(int i=0; i<n; i++) {
 			for(int j=0; j<m; j++) {
+				//도화지에 그림이 있고, 방문하지 않았다면
 				if(paint[i][j]==1 && !visited[i][j]) {
-					cnt++;
-					size = 0;
-					dfs(i,j);
-					max = Math.max(max, size);
+					cnt++; //그림개수 세기
+					size = 0; //그림크기 0으로 초기화
+					dfs(i,j); //dfs호출
+					max = Math.max(max, size); //size가 max보다 더 크면 max값 size값으로 초기화
 				}
 			}
 		}
-		System.out.println(cnt);
-		System.out.println(max);
+		System.out.println(cnt); //총 그림개수 출력
+		System.out.println(max); //가장 큰 그림 크기 출력
 	}
 	
 	
@@ -58,9 +55,9 @@ public class Main {
 	//dfs
 	static void dfs(int r, int c) {
 		visited[r][c] = true; //방문처리
-		size++;
+		size++; //그림 크기 추가
 		
-		//인접노드 찾기
+		//그림 크기 탐색
 		for(int i=0; i<4; i++) {
 			int nr = r+dr[i];
 			int nc = c+dc[i];
