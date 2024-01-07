84aa9a25b87624b39cb791161c654aa28f0ac631
hjung43 <140682638+hjung43@users.noreply.github.com>
2024-01-08 02:22:24 +0900

    [Silver I] Title: 그림, Time: 392 ms, Memory: 51132 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,59 +1,73 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int n, m, cnt, pSize, mSize;
-	static int[][] picture;
+	static int n,m,cnt,max,size;
+	static int[][] paint;
 	static boolean[][] visited;
-	// 델타 (상,우,하,좌)
-	static int[] dr = { -1, 0, 1, 0 };
-	static int[] dc = { 0, 1, 0, -1 };
-
+	
+	//좌,상,우,하
+	static int[]dr = {0,-1,0,1};
+	static int[]dc = {-1,0,1,0};
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st1 = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st1.nextToken());
-		m = Integer.parseInt(st1.nextToken());
-		picture = new int[n][m];
-		visited = new boolean[n][m];
-		cnt = 0; // 그림개수
-		mSize = 0; // 가장 큰 그림 크기
-		for (int i = 0; i < n; i++) {
-			StringTokenizer st2 = new StringTokenizer(br.readLine());
-			for (int j = 0; j < m; j++) {
-				picture[i][j] = Integer.parseInt(st2.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken()); //도화지 세로크기
+		m = Integer.parseInt(st.nextToken()); //도화지 가로크기
+		
+		paint = new int[n][m]; //그림배열
+		visited = new boolean[n][m]; //방문배열
+		
+		//그림 입력받기
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<m; j++) {
+				paint[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (picture[i][j] == 1 && !visited[i][j]) {
+		
+		cnt = 0; //그림개수
+		max = 0; //가장 큰 그림의 크기
+		
+		//그림 찾기
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				if(paint[i][j]==1 && !visited[i][j]) {
 					cnt++;
-					pSize = 1;
-					dfs(i, j);
-					mSize = Math.max(pSize, mSize);
+					size = 0;
+					dfs(i,j);
+					max = Math.max(max, size);
 				}
 			}
 		}
 		System.out.println(cnt);
-		System.out.println(mSize);
-
-
+		System.out.println(max);
 	}
-
-	public static void dfs(int pr, int pc) {
-		visited[pr][pc] = true;
-		for (int i = 0; i < 4; i++) {
-			int nr = pr + dr[i];
-			int nc = pc + dc[i];
-
-			if (nr >= 0 && nr < n && nc >= 0 && nc < m && picture[nr][nc] == 1 && !visited[nr][nc]) {
-				pSize++;
-				dfs(nr, nc);
+	
+	
+	
+	
+	//dfs
+	static void dfs(int r, int c) {
+		visited[r][c] = true; //방문처리
+		size++;
+		
+		//인접노드 찾기
+		for(int i=0; i<4; i++) {
+			int nr = r+dr[i];
+			int nc = c+dc[i];
+			//다음칸이 도화지 안이고 그림이있으며 방문하지 않았을 때
+			if(nr>=0 && nr<n && nc>=0 && nc<m && paint[nr][nc]==1 && !visited[nr][nc]) {
+				dfs(nr,nc);
 			}
-
 		}
 	}
-
 }
