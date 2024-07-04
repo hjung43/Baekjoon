6c4d5a53e53940d9a6bcffc545109bf78c8d1977
유호정 <140682638+hjung43@users.noreply.github.com>
2024-07-04 17:03:57 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 112 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,17 +1,23 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-				
-		for(int i = 1; i<=n; i++) {
-			for(int k =n-i; k>0; k--) {
-				System.out.print(" ");
-			}
-			for(int j = 0; j<i; j++) {
-				System.out.print("*");
-			}System.out.println();
-		}
-	}
+	public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        for(int i=0; i<n; i++) {
+        	StringBuilder sb = new StringBuilder();
+        	for(int j=0; j<n-(i+1); j++) {
+        		sb.append(" ");
+        	}
+        	for(int k=0; k<i+1; k++) {
+        		sb.append("*");
+        	}
+        	System.out.println(sb);
+        }
+    }
+
 }
