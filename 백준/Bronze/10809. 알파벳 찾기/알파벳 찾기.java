0259eafbd1f4cbf6c29b7f151b4da94587be761b
유호정 <140682638+hjung43@users.noreply.github.com>
2024-09-26 15:16:19 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 112 ms, Memory: 15892 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,21 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String str = sc.next(); //문자열 입력받기
-		int [] alphabet = new int[26]; //알파벳 위치 저장할 문자열
-		Arrays.fill(alphabet,-1); //알파벳 문자열 모든 인덱스 -1로 초기화
-								  //(arr.fill(arr,초기화값) = 모든 인덱스의 값을 초기화값으로 바꿈)
-		//알파벳 위치 alphabet배열에 저장하기
-		for(int i=0; i<str.length(); i++) {
-			if(alphabet[str.charAt(i)-97]==-1) //같은 문자일때는 처음 위치를 저장하기 위해 -1일때만 위치 수정
-				alphabet[str.charAt(i)-97] = i; //str에서 한글자씩 문자로 나눠 a의 아스키코드값(97) 빼기
-		}
-		//결과 출력
-		for(int i=0; i<26; i++) 
-			System.out.print(alphabet[i]+" ");
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int[] alp = new int[26];
+        Arrays.fill(alp, -1);
+        int idx = 0;
+        for(int i=0; i<s.length(); i++){
+            int spell = s.charAt(i)-'a';
+            if(alp[spell]==-1){
+                alp[spell]=idx;
+            }
+            idx++;
+        }
 
+        for(int i=0; i<alp.length; i++){
+                System.out.print(alp[i]+" ");
+        }
+
+
+    }
 }
