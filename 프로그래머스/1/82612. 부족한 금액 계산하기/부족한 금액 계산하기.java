2b7806a66bec84c9397fb2c5ef99d991faadec45
유호정 <140682638+hjung43@users.noreply.github.com>
2025-01-22 09:11:57 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.19 ms, Memory: 90.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long total = 0; //총 놀이기구의 이용 금액
+        //한번 탈때마다 n배 해서 더하기
+        for(int i=0; i<count; i++){
+            total += price*(i+1);
+        }
+        //금액이 부족하지 않으면 0리턴
+        if(money-total>=0){
+            return 0;
+        }
+        return total-money;
+    }
+}
