ce68ab7622a4c5b22f0edd64e8fbe88776e9da18
유호정 <140682638+hjung43@users.noreply.github.com>
2025-03-20 17:23:00 +0900

    [Silver II] Title: 유기농 배추, Time: 144 ms, Memory: 15972 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,56 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+import java.lang.*;
 
 public class Main {
-	static int r,c,result;
-	static int[][]farm;
-	static boolean[][]visited;
-	//델타 (상,우,하,좌)
-	static int[]dr = {-1,0,1,0};
-	static int[]dc = {0,1,0,-1};
-	
     public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		for(int t=1; t<=T; t++) {
-			StringTokenizer st1 = new StringTokenizer(br.readLine());
-			r = Integer.parseInt(st1.nextToken());
-			c = Integer.parseInt(st1.nextToken());
-			int n = Integer.parseInt(st1.nextToken());
-			farm = new int[r][c];
-			visited = new boolean[r][c];
-			for(int i=0; i<n; i++) {
-				StringTokenizer st2 = new StringTokenizer(br.readLine());
-				int cabR = Integer.parseInt(st2.nextToken());
-				int cabC = Integer.parseInt(st2.nextToken());
-				farm[cabR][cabC] = 1;
-			}
-			result = 0;
-			for(int i=0; i<r; i++) {
-				for(int j=0; j<c; j++) {
-					if(farm[i][j]==1 && !visited[i][j]) {
-						result++;
-						dfs(i,j);
-					}
-				}
-			}
-			System.out.println(result);
-		}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        int[] dx = {1,0,-1,0};
+        int[] dy = {0,1,0,-1};
+        for(int t=0; t<T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int m = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            int[][] map = new int[m][n];
+            for(int i = 0; i < k; i++) {
+                st = new StringTokenizer(br.readLine());
+                map[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = 1;
+            }
+            int ans = 0;
+            for(int i = 0; i < m; i++) {
+                for(int j = 0; j < n; j++) {
+                    if(map[i][j] == 1) {
+                        ans++;
+                        Queue<int[]> q = new LinkedList<>();
+                        q.offer(new int[]{i, j});
+                        while(!q.isEmpty()) {
+                            int[]now = q.poll();
+                            for(int l = 0; l < 4; l++) {
+                                int nx = now[0] + dx[l];
+                                int ny = now[1] + dy[l];
+                                if(nx>=0 && nx<m && ny>=0 && ny<n && map[nx][ny] == 1) {
+                                    q.offer(new int[]{nx, ny});
+                                    map[nx][ny] = 0;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            System.out.println(ans);
+        }
     }
-    
-    public static void dfs (int cabR, int cabC) {
-    	visited[cabR][cabC] = true;
-    	for(int i=0; i<4; i++) {
-    		int nr = cabR+dr[i];
-    		int nc = cabC+dc[i];
-    		
-    		if(nr>=0 && nr<r && nc>=0 && nc<c && farm[nr][nc]==1 && !visited[nr][nc]) {
-    			dfs(nr,nc);
-    		}
-    		
-    	}
-    }
-	
 }
