0360a97320c122590a8625f41abd9855a852fbdf
유호정 <140682638+hjung43@users.noreply.github.com>
2025-03-20 18:49:08 +0900

    [Silver I] Title: 단지번호붙이기, Time: 108 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,61 +1,50 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+import java.lang.*;
 
 public class Main {
-	static int n, cnt, size;
-	static int[][] apt;
-	static boolean[][] visited;
-	static int[]dr = {-1,0,1,0};
-	static int[]dc = {0,1,0,-1};
-
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		apt = new int[n][n];
-		visited = new boolean[n][n];
-		cnt = 0;
-		
-		for(int i=0; i<n; i++) {
-			String str = br.readLine();
-			for(int j=0; j<n; j++) {
-				apt[i][j] = str.charAt(j)-'0';
-			}
-		}
-		
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n; j++) {
-				if(apt[i][j]==1 && !visited[i][j]) {
-					cnt++;
-					size = 1;
-					dfs(i,j);
-					pq.offer(size);
-					
-				}
-			}
-		}
-		System.out.println(cnt);
-		for(int i=0; i<cnt; i++) {
-			System.out.println(pq.poll());
-		}
-		
-	}
-	public static void dfs(int r, int c) {
-		visited[r][c] = true;
-		for(int i=0; i<4; i++) {
-			int nr = r+dr[i];
-			int nc = c+dc[i];
-			if(nr>=0 && nr<n && nc>=0 && nc<n && apt[nr][nc]==1 && !visited[nr][nc]) {
-				size++;
-				dfs(nr,nc);
-			}
-		}
-	}
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] apt = new int[n][n];
+        for(int i=0; i<n; i++){
+            String str = br.readLine();
+            for(int j=0; j<n; j++){
+                apt[i][j] = str.charAt(j)-'0';
+            }
+        }
+        int[] dh = {0,1,0,-1};
+        int[] dw = {1,0,-1,0};
+        int cnt = 0;
+        List<Integer>list = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(apt[i][j]==1){
+                    cnt++;
+                    Queue<int[]>q = new LinkedList<>();
+                    int size = 1;
+                    q.offer(new int[]{i,j});
+                    apt[i][j] = 0;
+                    while(!q.isEmpty()){
+                        int[] now = q.poll();
+                        for(int k=0; k<4; k++){
+                            int nh = now[0]+dh[k];
+                            int nw = now[1]+dw[k];
+                            if(nh>=0 && nh<n && nw>=0 && nw<n && apt[nh][nw]==1){
+                                q.offer(new int[]{nh,nw});
+                                apt[nh][nw] = 0;
+                                size++;
+                            }
+                        }
+                    }
+                    list.add(size);
+                }
+            }
+        }
+        System.out.println(cnt);
+        Collections.sort(list);
+        for(int i=0; i<list.size(); i++){
+            System.out.println(list.get(i));
+        }
+    }
 }
