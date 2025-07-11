036482053b6e6fc41de30aecf8afe3023acfc4ce
유호정 <140682638+hjung43@users.noreply.github.com>
2025-07-11 17:53:35 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 13916 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,13 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
 public class Main {
-	public static void main(String[] args) {
-		
-        
-        System.out.println("\\    /\\");
-        System.out.println(" )  ( ')");
-        System.out.println("(  /  )");
-        System.out.println(" \\(__)|");
-        
-
-	}
-
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\").append("\n")
+            .append(" )  ( ')").append("\n")
+            .append("(  /  )").append("\n")
+            .append(" \\(__)|");
+        System.out.println(sb);
+    }
 }
