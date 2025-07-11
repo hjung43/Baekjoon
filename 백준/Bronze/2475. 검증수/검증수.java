2d9c76243d86527debd833b9fb37821238485516
유호정 <140682638+hjung43@users.noreply.github.com>
2025-07-11 18:05:24 +0900

    [Bronze V] Title: 검증수, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int sum = 0;
+        for(int i=0; i<5; i++){
+            int n = Integer.parseInt(st.nextToken());
+            sum += (n*n);
+        }
+        System.out.print(sum%10);
+    }
+}
