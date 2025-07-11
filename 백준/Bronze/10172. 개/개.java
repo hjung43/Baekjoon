0e7fe36a9ee4825445d1a2f8e79ef1ffd48c192d
유호정 <140682638+hjung43@users.noreply.github.com>
2025-07-11 17:51:41 +0900

    [Bronze V] Title: 개, Time: 100 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("|\\_/|").append("\n")
+            .append("|q p|   /}").append("\n")
+            .append("( 0 )\"\"\"\\").append("\n")
+            .append("|\"^\"`    |").append("\n")
+            .append("||_/=\\\\__|");
+        System.out.println(sb);
+    }
+}
