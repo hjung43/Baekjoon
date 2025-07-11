278bc355e6d8a1a5751e012c565dd80345f69336
유호정 <140682638+hjung43@users.noreply.github.com>
2025-07-11 18:07:35 +0900

    [Bronze V] Title: N 찍기, Time: 744 ms, Memory: 30596 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for(int i=1; i<=n; i++){
+            System.out.println(i);
+        }
+    }
+}
