539680414b9fb4e70b686742c3ffdbda5ada6117
유호정 <140682638+hjung43@users.noreply.github.com>
2025-07-11 16:57:24 +0900

    [Bronze V] Title: 구구단, Time: 108 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,12 +1,16 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.lang.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		
-		for(int i = 1; i<10; i++) {
-			System.out.println(N+" * "+i+" = "+(N*i));
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<10; i++){
+            int prod = n * i;
+            sb.append(n).append(" * ").append(i).append(" = ").append(prod).append("\n");
+        }
+        System.out.println(sb);
+    }
 }
